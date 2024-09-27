8980dbd4dad675f3f270146539519c857a3ca339
nibu-grg <20047500@mydbs.ie>
2024-09-27 14:12:16 +0530

    js

--- euler.js
+++ euler.js
@@ -1,11 +1,13 @@
-a=3;
-b=5;
-c=1000;
-s=0;
-for(i=1;i<1000;i++)
-{
-    if(i%3==0 || i%5==0)
-        s+=i;
-}
-alert("Sum is "+s);
-console.log(s);
+let sum=()=>
+    {
+        num1 = document.getElementById("num1").value;
+        num2 = document.getElementById("num2").value;
+        limit = document.getElementById("limit").value;
+        s=0;
+        for(i=1;i<limit;i++)
+        {
+            if(i%num1==0 || i%num2==0)
+                s+=i;
+        }
+        alert("Sum = "+s);
+    }
