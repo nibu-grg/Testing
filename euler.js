f723eef27150678e19f1a01000b5c85b2a7f7ed5
nibu-grg <20047500@mydbs.ie>
2024-09-28 13:54:03 +0530

    sum

--- euler.js
+++ euler.js
@@ -1,13 +1,18 @@
-let sum=()=>
+function sum(a,b,n)
     {
-        num1 = document.getElementById("num1").value;
-        num2 = document.getElementById("num2").value;
-        limit = document.getElementById("limit").value;
         s=0;
-        for(i=1;i<limit;i++)
+        for(i=1;i<n;i++)
         {
-            if(i%num1==0 || i%num2==0)
+            if(i%a==0 || i%b==0)
                 s+=i;
         }
         alert("Sum = "+s);
     }
+
+function GetValues()
+{
+    num1 = document.getElementById("num1").value;
+    num2 = document.getElementById("num2").value;
+    limit = document.getElementById("limit").value;
+    sum(num1,num2,limit);
+}
