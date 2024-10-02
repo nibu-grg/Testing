0e18fe0e6d379a90f7952b13db1fa9d35e6abcb1
nibu-grg <20047500@mydbs.ie>
2024-10-02 21:09:28 +0530

    input hide

--- euler.js
+++ euler.js
@@ -15,13 +15,17 @@ function GetValues()
     num2 = document.getElementById("num2").value;
     limit = document.getElementById("limit").value;
     sum(num1,num2,limit);
+    document.getElementById("num1").style.display="none";
+    document.getElementById("num2").style.display="none";
+    document.getElementById("limit").style.display="none";
+    document.getElementById("sum")..style.display="none";
 }
 
-function Hide()
-{
-    document.getElementById("div1").style.display="none";
-}
+
 function Show()
 {
-    document.getElementById("div1").style.display="block";
+    document.getElementById("num1").style.display="block";
+    document.getElementById("num2").style.display="block";
+    document.getElementById("limit").style.display="block";
+    document.getElementById("sum")..style.display="block";
 }
